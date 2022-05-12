819014b7116b5b5766d132e7e3bb73d08d41742c
yushurygina <yushurygina@phoenixit.ru>
2022-05-12 18:54:28 +0300

    inint app

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true, // автоматическая точка с запятой
+  singleQuote: false, // все двойные кавычки будут преобразованы в одинарные
+  printWidth: 100, // длина строки
+  trailingComma: "all", // обеспечивает наличие запятой после последнего свойства объекта.
+  bracketSpacing: true, // отвечает за вставку пробелов между телом объекта и фигурными скобками в объектных литералах
+  jsxBracketSameLine: false,
+};

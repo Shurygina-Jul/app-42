45f7c09daae0fa6db055ba89acc06ba19af31867
Shurygina Julia <melam.2916@gmail.com>
2024-02-01 15:00:25 +0300

    husky lint-staged  добавим

--- src/hoc/withErrorApi.tsx
+++ src/hoc/withErrorApi.tsx
@@ -4,7 +4,7 @@ import ErrorMessage from "components/ErrorMessage";
 import { IErrorApi } from "types/types";
 //import { ICharacter } from "types/types";
 
-//console.log(123)
+//console.log(123);
 
 export const withErrorApi = (View: React.ComponentType<IErrorApi>) => {
   const test = "unusebale var";
